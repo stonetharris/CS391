3e072d8f797564726861c6c5793ce14a5bd68cac
Stone Harris <stonetharris@gmail.com>
2024-03-22 13:35:36 -0400

    Did discussion exercise

---
+++ dis-7/src/AgeVerification.jsx
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+export default function AgeVerification() {
+    const [age, setAge] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [welcomeMessageVisible, setWelcomeMessageVisible] = useState(false);
+
+    const checkAge = () => {
+        const ageInput = parseInt(age);
+
+        if (isNaN(ageInput) || ageInput < 0) {
+            setErrorMessage('Your answer is not acceptable! Please put a new one!');
+            setAge('');
+        } else if (ageInput > 17 && ageInput <= 120) {
+            setWelcomeMessageVisible(true);
+            setErrorMessage('');
+        } else if (ageInput < 18) {
+            setErrorMessage('You must be at least 18 years old to enter.');
+        } else if (ageInput > 120) {
+            setErrorMessage('Age cannot be more than 120 years.');
+        }
+    };
+
+    function handleAgeChange(event){
+        setAge(event.target.value);
+        setWelcomeMessageVisible(false);
+        setErrorMessage('');
+    }
+
+    return (
+        <div>
+            <h2>To enter this website, you need to be at least 18 years old.</h2>
+            <label htmlFor="age">How old are you? </label>
+            <input id="age" value={age} onChange={handleAgeChange} />
+            <button onClick={checkAge}>Check Age</button>
+            {errorMessage && <h5>{errorMessage}</h5>}
+            {welcomeMessageVisible && <h1>Welcome</h1>}
+        </div>
+    );
+}
+
