03d14ade1629adb9061568d22be65f796a4ecdb7
Stone Harris <stonetharris@gmail.com>
2024-03-24 12:01:49 -0400

    Fixed styling

--- projects/mini-projects/mp4/pages/_app.js
+++ projects/mini-projects/mp4/pages/_app.js
@@ -1,15 +1,48 @@
 import { createGlobalStyle } from 'styled-components';
 import Header from '../components/Header';
 
+const googleFontURL = 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap';
+
+
 const GlobalStyle = createGlobalStyle`
+    @import url('${googleFontURL}'); 
+
+    :root {
+        --padding-small: 8px;
+        --padding-medium: 16px;
+        --padding-large: 24px;
+        --margin-small: 8px;
+        --margin-medium: 16px;
+        --margin-large: 24px;
+    }
+
     body {
         margin: 0;
         padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-        "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-        sans-serif;
+        font-family: 'Open Sans', sans-serif; 
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        font-size: calc(12px + 0.5vw); 
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+    
+    @media (max-width: 768px) {
+        :root {
+            --padding-small: 4px;
+            --padding-medium: 8px;
+            --padding-large: 12px;
+            --margin-small: 4px;
+            --margin-medium: 8px;
+            --margin-large: 12px;
+        }
+
+        body {
+            font-size: calc(12px + 1vw); 
+        }
     }
 `;
 
